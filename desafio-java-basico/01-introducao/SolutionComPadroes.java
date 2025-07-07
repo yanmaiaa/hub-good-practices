1530081fd158e33488db7c21494254244888ab7d
Yan Maia <yandamasceno01@gmail.com>
2025-07-07 20:56:19 -0300

    feat: desafio 01 do if-else

---
+++ desafio-java-basico/01-introducao/SolutionComPadroes.java
@@ -0,0 +1,44 @@
+
+//Aqui vamos refatorar o código para facilitar a legibilidade melhorando a maneira como o escrevemos.
+//O que vamos usar: Princípio da Responsabilidade Única para criar métodos que tenham apenas uma única responsabilidade, Object Calisthenics onde passaremos a não usar mais o else, pois a ideia é usar o if com retorno já que a premissa aqui é o retorno antecipado evitando complexidade excessiva de blocos else aninhados
+//além de praticas vistas no Clean Code onde devemos usar nomes de métodos intuitivos e que descrevem exatamente o que eles fazem. Tem outras abordagens, mas para faciltiar a sintaxe iremos fazer tudo aqui nesta classe.
+
+import java.util.Scanner;
+
+public class SolutionComPadroes {
+  private static final Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args){
+    int n = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    scanner.close();
+    imprimirClassificacaoNumerica(n);
+  }
+
+  private static void imprimirClassificacaoNumerica(int n){
+    String intervaloNumerico = verificaIntervaloNumerico(n);
+      if(numeroEhPar(n) && intervaloNumerico.equals("intervalo2A5") ||
+          numeroEhPar(n) && intervaloNumerico.equals("maiorQue20")){
+            System.out.println("Not Weird");
+            return;
+      }
+      
+      if (numeroEhPar(n) && intervaloNumerico.equals("intervalo6A20") ||
+          !numeroEhPar(n)) {
+        System.out.println("Weird");
+        return;
+      }
+  }
+
+  private static String verificaIntervaloNumerico(int num){
+    if(num >= 2 && num <= 5) return "intervalo2A5";
+    if(num >= 6 && num <= 20) return "intervalo6A20";
+    if(num > 20) return "maiorQue20";
+    return "foraDoEscopo";
+  }
+
+  private static boolean numeroEhPar(int num){
+    return num % 2 == 0;
+  }  
+
+  
+}
---
+++ desafio-java-basico/01-introducao/if-else/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * Task
+Given an integer, , perform the following conditional actions:
+
+If N is odd, print "Weird"
+If N is even and in the inclusive range of 2 to 5, print "Not Weird"
+If N is even and in the inclusive range of 6 to 20 , print "Weird"
+If N is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not N is "weird".
+
+Input Format
+
+A single line containing a positive integer, .
+ */
+
+ //Abaixo está uma solução básica sem usar boas práticas para deixar o código mais legível e manutenível.
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        //Nos if's temos estruturas que se repetem e poderiam ser agrupadas já que existem condições que deveriam retornar o mesmo resultado impresso no console. Com isso, podemos ajustar de forma mais eficiente na próxima classe.
+        if(n % 2 == 0 && n >= 2 && n <= 5){ 
+            System.out.println("Not Weird");
+        }else if(n % 2 == 0 && n >= 6 && n <= 20){
+            System.out.println("Weird");
+        }else if(n % 2 == 0 &&  n > 20){
+            System.out.println("Not Weird");
+        }else{
+            System.out.println("Weird");
+        }
+
+        scanner.close();
+    }
+
+}
